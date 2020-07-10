e733b0453d1889695d4ef373ec6860550496c669
dsuriani <32660731+dsuriani@users.noreply.github.com>
2020-07-10 13:51:20 +0800

    New ParkingLot3 on 10/07/2020 1:50pm reason: need Multiple ParkingLot Type

---
+++ ParkingLot3.java
@@ -0,0 +1,8 @@
+public class ParkingLot3 {
+  public static void main(String[] args) {
+    printMethodParkingLot2();
+  }
+  public static void printMethodParkingLot2() {
+	 System.out.println("ParkingLot System In Petaling Jaya1234567890");
+  }
+}
